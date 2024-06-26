45036635b61e290444d5b5f6c8052488a3625ce1
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2024-06-25 20:12:04 -0700

    Simplify configuration (#1)
    * Include src/*.ts files in tsconfig files
    
    * Upgrade several packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,51 +1,17 @@
-import js from '@eslint/js';
-import tsParser from '@typescript-eslint/parser';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import { fixupConfigRules } from '@eslint/compat';
+import pluginJs from '@eslint/js';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import ts from 'typescript-eslint';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import tseslint from 'typescript-eslint';
 
 export default [
-  js.configs.recommended,
-  ...ts.configs.strict, // https://typescript-eslint.io/getting-started/
-  { ignores: ['**/dist', '**/out', '**/eslint.config.mjs'] },
-  {
-    plugins: { 'react-hooks': reactHooks, 'react-refresh': reactRefresh },
-    languageOptions: {
-      globals: { ...globals.browser },
-      parser: tsParser,
-      ecmaVersion: 5,
-      sourceType: 'script',
-      parserOptions: {
-        project: [
-          path.join(__dirname, 'tsconfig.json'),
-          path.join(__dirname, 'tsconfig.node.json'),
-          path.join(__dirname, 'tsconfig.web.json'),
-        ],
-      },
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules, // https://stackoverflow.com/q/76183413
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      '@typescript-eslint/restrict-template-expressions': [
-        'error',
-        { allowBoolean: true, allowNumber: true },
-      ],
-      '@typescript-eslint/no-floating-promises': 'error',
-      '@typescript-eslint/no-confusing-void-expression': 'off',
-      '@typescript-eslint/no-non-null-assertion': 'off',
-      '@typescript-eslint/no-misused-promises': [
-        'error',
-        { checksVoidReturn: false },
-      ],
-    },
-  },
+  { settings: { react: { version: 'detect' } } },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { ignores: ['**/dist', '**/out'] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  { rules: { 'react/react-in-jsx-scope': 'off' } },
 ];
